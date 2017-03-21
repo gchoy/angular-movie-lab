d7fa6f7670b4a1f63fac8504a2a0825f0b8a700a
gchoy <gabriela.choy@gmail.com>
2017-03-20 19:08:49 -0700

    added repeat functionality to html

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
 angular
-  .module('movieApp',[]);
+  .module('movieApp',[])
   .controller('movieController', movieController);
 
   function movieController(){
     var vm=this;
 
-    vm.moviesTowatch =[
+    vm.moviesToWatch =[
         {
-          id: 1,
           name: 'Logan',
           genre: 'Action'
         },
         {
-          id: 2,
+
           name: 'Moana',
           genre: 'Animated'
         },
         {
-          id: 3,
+
           name: 'Fences',
           genre: 'Drama'
         },
         {
-          id: 4,
+
           name: 'Hidden Figures',
           genre: 'Drama'
         },
         {
-          id: 5,
+
           name: 'Moonlight',
           genre: 'Drama'
         },
         {
-          id: 6,
+
           name: 'John Wick 2',
           genre: 'Action'
         }
